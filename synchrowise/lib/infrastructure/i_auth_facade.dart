e5dc5c98aa87a6d90f93062e2e9de9127e0147e5
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2022-03-13 18:44:19 +0300

    update

--- synchrowise/lib/infrastructure/i_auth_facade.dart
+++ synchrowise/lib/infrastructure/i_auth_facade.dart
@@ -3,6 +3,6 @@ import 'package:synchrowise/domain/auth/synchrowise_user.dart';
 import 'package:synchrowise/infrastructure/failures/auth_failure.dart';
 
 abstract class IAuthFacade {
-  Future<Either<AuthFailure, Unit>> signInWithGoogleAuth();
+  Future<Either<AuthFailure, SynchrowiseUser>> signInWithGoogleAuth();
   Future<Either<AuthFailure, SynchrowiseUser>> getSignedInUser();
 }
