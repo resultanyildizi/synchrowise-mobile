8084a5e6ebb1568508ffa2183000c918e9d28fc7
Selim Gülce <43720773+gulceselim@users.noreply.github.com>
2022-03-27 21:59:51 +0300

    register start page

---
+++ synchrowise/lib/presentation/auth/register_steps/register_steps_start.dart
@@ -0,0 +1,56 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:synchrowise/constants.dart';
+import 'package:synchrowise/presentation/auth/register_steps/register_steps_1.dart';
+import 'package:synchrowise/presentation/helpers/default_button.dart';
+
+class RegisterStepsStart extends StatelessWidget {
+  const RegisterStepsStart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/image_2.png",
+              width: 350,
+              height: 358,
+            ),
+            Text("almost_done".tr(),
+                style: Theme.of(context).textTheme.headline1),
+            const SizedBox(height: 8),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 70),
+              child: Text(
+                "almost_done_desc".tr(),
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
+            ),
+            const Spacer(),
+            DefaultButton(
+              backgroundColor: primaryColor,
+              borderColor: null,
+              textColor: kcWhiteColor,
+              text: "continue".tr(),
+              padding: 35,
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const RegisterSteps1(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 80),
+          ],
+        ),
+      ),
+    );
+  }
+}
