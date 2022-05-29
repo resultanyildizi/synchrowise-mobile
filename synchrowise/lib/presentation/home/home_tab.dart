415530d80c54a77abc95e5d561d963b3803476ac
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2022-05-29 21:14:31 +0300

    get group bloc

--- synchrowise/lib/presentation/home/home_tab.dart
+++ synchrowise/lib/presentation/home/home_tab.dart
@@ -1,7 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:synchrowise/application/group_bloc/bloc/get_group_bloc.dart';
+import 'package:synchrowise/application/group_bloc/get_group_bloc/get_group_bloc.dart';
 import 'package:synchrowise/constants.dart';
 import 'package:synchrowise/domain/auth/synchrowise_user.dart';
 import 'package:synchrowise/infrastructure/group/group_repository/failure/group_repository_failure.dart';
