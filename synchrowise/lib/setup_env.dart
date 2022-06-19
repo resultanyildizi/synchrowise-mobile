b521296ad30f67aa9ad6ef462d217272933f85a9
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2022-06-19 14:45:53 +0300

    update

--- synchrowise/lib/setup_env.dart
+++ synchrowise/lib/setup_env.dart
@@ -5,10 +5,12 @@ late final String socketurl;
 
 Future<void> setupEnv() async {
   await dotenv.load();
-  final produrl = dotenv.get("API_PROD_URL");
-  final testurl = dotenv.get("API_TEST_URL");
+  final apiprodurl = dotenv.get("API_PROD_URL");
+  final apitesturl = dotenv.get("API_TEST_URL");
+  final sockeprodturl = dotenv.get("SOCKET_PROD_URL");
+  final socketesturl = dotenv.get("SOCKET_TEST_URL");
   final env = dotenv.get("ENV");
 
-  apiurl = env == "prod" ? produrl : testurl;
-  socketurl = dotenv.get("SOCKET_URL");
+  apiurl = env == "prod" ? apiprodurl : apitesturl;
+  socketurl = env == "prod" ? sockeprodturl : socketesturl;
 }
