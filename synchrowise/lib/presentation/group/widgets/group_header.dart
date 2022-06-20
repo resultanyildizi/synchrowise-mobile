b5390e4fc9bf8e07ed27ebc919ea61fb61e8aa3d
Nurettin Resul Tanyıldızı <tanyildiziresul@gmail.com>
2022-06-20 12:23:54 +0300

    Update group_header.dart

--- synchrowise/lib/presentation/group/widgets/group_header.dart
+++ synchrowise/lib/presentation/group/widgets/group_header.dart
@@ -1,7 +1,9 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:synchrowise/application/core/share_content_bloc/share_content_bloc.dart';
+import 'package:synchrowise/application/group/group_session_bloc/group_session_bloc.dart';
 import 'package:synchrowise/constants.dart';
 import 'package:synchrowise/domain/group/group_data.dart';
 import 'package:synchrowise/injection.dart';
@@ -16,85 +18,94 @@ class GroupHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     final groupData = context.read<GroupData>();
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Flexible(
-            flex: 8,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  groupData.groupKey,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6!
-                      .copyWith(fontSize: 20),
-                ),
-                Text(
-                  groupData.groupDesc,
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6!
-                      .copyWith(fontSize: 14),
-                ),
-                RichText(
-                    text: TextSpan(
+    return BlocBuilder<GroupSessionBloc, GroupSessionState>(
+      builder: (context, state) {
+        final memberCount = state.membersOption.fold(
+          () => 0,
+          (members) => members.size,
+        );
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                flex: 8,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TextSpan(
-                      text: (groupData.members.size > 1
-                              ? "x_people_in_group"
-                              : "x_person_in_group")
-                          .tr(
-                        namedArgs: {
-                          "count": groupData.members.size.toString(),
-                        },
-                      ),
+                    Text(
+                      groupData.groupKey,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                       style: Theme.of(context)
                           .textTheme
                           .headline6!
-                          .copyWith(color: grayDarkColor2),
+                          .copyWith(fontSize: 20),
                     ),
-                    TextSpan(
-                      text: " · ",
+                    Text(
+                      groupData.groupDesc,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                       style: Theme.of(context)
                           .textTheme
                           .headline6!
-                          .copyWith(color: grayDarkColor2),
+                          .copyWith(fontSize: 14),
                     ),
-                    TextSpan(
-                      text: convertToString(groupData.createdAt),
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline6!
-                          .copyWith(color: grayDarkColor2),
-                    ),
+                    RichText(
+                        text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: (memberCount > 1
+                                  ? "x_people_in_group"
+                                  : "x_person_in_group")
+                              .tr(
+                            namedArgs: {
+                              "count": memberCount.toString(),
+                            },
+                          ),
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(color: grayDarkColor2),
+                        ),
+                        TextSpan(
+                          text: " · ",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(color: grayDarkColor2),
+                        ),
+                        TextSpan(
+                          text: convertToString(groupData.createdAt),
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline6!
+                              .copyWith(color: grayDarkColor2),
+                        ),
+                      ],
+                    )),
                   ],
-                )),
-              ],
-            ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: CustomAnimatedButton(
+                  onTap: () {
+                    getIt<ShareContentBloc>().share(
+                      text:
+                          "${groupData.groupUrl}\n\nAramıza katılmak için bağlantı linkine tıklayabilirsin!",
+                      subject: "Synchrowise grup daveti",
+                    );
+                  },
+                  child: const Icon(Icons.share),
+                ),
+              ),
+            ],
           ),
-          Flexible(
-            flex: 1,
-            child: CustomAnimatedButton(
-              onTap: () {
-                getIt<ShareContentBloc>().share(
-                  text:
-                      "${groupData.groupUrl}\n\nAramıza katılmak için bağlantı linkine tıklayabilirsin!",
-                  subject: "Synchrowise grup daveti",
-                );
-              },
-              child: const Icon(Icons.share),
-            ),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
