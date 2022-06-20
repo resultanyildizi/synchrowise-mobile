bd3db86c269ce4b4f26a0715439583bcec10d3b5
Selim Gülce <43720773+gulceselim@users.noreply.github.com>
2022-06-20 11:51:41 +0300

    Update group_participant.dart

--- synchrowise/lib/presentation/group/widgets/group_participant.dart
+++ synchrowise/lib/presentation/group/widgets/group_participant.dart
@@ -1,9 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kt_dart/kt.dart';
 import 'package:provider/provider.dart';
 import 'package:synchrowise/application/group/group_session_bloc/group_session_bloc.dart';
 import 'package:synchrowise/constants.dart';
+import 'package:synchrowise/domain/auth/user_summary.dart';
 import 'package:synchrowise/domain/group/group_data.dart';
 import 'package:synchrowise/extensions/build_context_ext.dart';
 import 'package:synchrowise/presentation/core/widgets/close_icon.dart';
@@ -49,92 +52,100 @@ class ParticipantBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final groupData = context.read<GroupData>();
 
-    return Expanded(
-      child: ListView.builder(
-        itemCount: groupData.members.size,
-        itemBuilder: (context, index) {
-          final member = groupData.members[index];
-          final isAdmin =
-              member.synchrowiseId == groupData.groupOwner.synchrowiseId;
-          final myId = context.synchrowiseUser.synchrowiseId;
+    return BlocBuilder<GroupSessionBloc, GroupSessionState>(
+      builder: (context, state) {
+        final members = state.membersOption.fold(
+          () => const KtList<UserSummary>.empty(),
+          (members) => members,
+        );
+        return Expanded(
+          child: ListView.builder(
+            itemCount: members.size,
+            itemBuilder: (context, index) {
+              final member = members[index];
+              final isAdmin =
+                  member.synchrowiseId == groupData.groupOwner.synchrowiseId;
+              final myId = context.synchrowiseUser.synchrowiseId;
 
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 10.0),
-            child: Container(
-              height: 60,
-              decoration: BoxDecoration(
-                color: grayLightColor,
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(
-                  color: Colors.transparent,
-                  width: 1.5,
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: CachedNetworkImage(
-                        width: 35,
-                        height: 35,
-                        imageUrl: member.avatar.getHttpsPath,
-                      ),
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: grayLightColor,
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.transparent,
+                      width: 1.5,
                     ),
-                    const SizedBox(width: 10),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Row(
                       children: [
-                        Text(
-                          member.username,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6!
-                              .copyWith(fontSize: 16),
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: CachedNetworkImage(
+                            width: 35,
+                            height: 35,
+                            imageUrl: member.avatar.getHttpsPath,
+                          ),
                         ),
-                        if (isAdmin)
-                          Text(
-                            isAdmin ? "admin".tr() : "",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline6!
-                                .copyWith(
-                                    fontStyle: FontStyle.italic,
-                                    color: grayDarkColor2),
+                        const SizedBox(width: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              member.username,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline6!
+                                  .copyWith(fontSize: 16),
+                            ),
+                            if (isAdmin)
+                              Text(
+                                isAdmin ? "admin".tr() : "",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline6!
+                                    .copyWith(
+                                        fontStyle: FontStyle.italic,
+                                        color: grayDarkColor2),
+                              ),
+                          ],
+                        ),
+                        const Spacer(),
+                        if (myId == groupData.groupOwner.synchrowiseId &&
+                            myId != member.synchrowiseId)
+                          CloseIcon(
+                            onTap: () {
+                              return synchrowisePopup(
+                                context,
+                                "delete_member".tr(),
+                                "delete_member_desc".tr(),
+                                "no".tr(),
+                                () => SynchrowiseNavigator.pop(context),
+                                "yes".tr(),
+                                () {
+                                  groupSessionBloc.deleteMember(
+                                    groupData: groupData,
+                                    member: member,
+                                  );
+                                  SynchrowiseNavigator.pop(context);
+                                },
+                              );
+                            },
                           ),
                       ],
                     ),
-                    const Spacer(),
-                    if (myId == groupData.groupOwner.synchrowiseId &&
-                        myId != member.synchrowiseId)
-                      CloseIcon(
-                        onTap: () {
-                          return synchrowisePopup(
-                            context,
-                            "delete_member".tr(),
-                            "delete_member_desc".tr(),
-                            "no".tr(),
-                            () => SynchrowiseNavigator.pop(context),
-                            "yes".tr(),
-                            () {
-                              groupSessionBloc.deleteMember(
-                                groupData: groupData,
-                                member: member,
-                              );
-                              SynchrowiseNavigator.pop(context);
-                            },
-                          );
-                        },
-                      ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          );
-        },
-      ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
